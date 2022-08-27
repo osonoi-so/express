ab56052be14bcae9bbdca0b204d05dcf2325bbd1
osonoi-so <68226770+osonoi-so@users.noreply.github.com>
2022-08-27 12:21:49 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express20220808' });
+  res.render('index', { title: 'Express20220827' });
 });
 
 module.exports = router;
